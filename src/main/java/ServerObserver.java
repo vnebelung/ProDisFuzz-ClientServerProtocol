a73668c972648050c6eda81cd985d8a2e6d756b6
Volker Nebelung <vnebelung@prodisfuzz.net>
2016-09-28 22:48:04 +0200

    - Javadoc completed - Include source files and Javadoc in build

--- src/main/java/ServerObserver.java
+++ src/main/java/ServerObserver.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of ProDisFuzz, modified on 27.09.16 22:16.
+ * This file is part of ProDisFuzz, modified on 28.09.16 22:46.
  * Copyright (c) 2013-2016 Volker Nebelung <vnebelung@prodisfuzz.net>
  * This work is free. You can redistribute it and/or modify it under the
  * terms of the Do What The Fuck You Want To Public License, Version 2,
@@ -7,18 +7,17 @@
  */
 
 import message.server.IncomingMessage;
-import protocol.ProtocolStateException;
 
-import java.io.IOException;
-
+/**
+ * This interface represents an observer of the server. The observer will be notified when the server receives a
+ * packet that must be processed.
+ */
 public interface ServerObserver {
 
     /**
      * Handles a client's message and sends the according response to the client.
      *
      * @param message the received message
-     * @throws IOException            if an I/O error occurs
-     * @throws ProtocolStateException if the protocol enters an invalid state
      */
     void handleMessage(IncomingMessage message);
 }
