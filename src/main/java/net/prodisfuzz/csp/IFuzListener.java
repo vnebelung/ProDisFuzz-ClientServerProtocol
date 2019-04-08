ec9dbe27125b6543eb34e1a2349807eb1a6bc08a
Volker Nebelung <vnebelung@prodisfuzz.net>
2019-04-08 19:41:59 +0200

    Updated javadoc

--- src/main/java/net/prodisfuzz/csp/IFuzListener.java
+++ src/main/java/net/prodisfuzz/csp/IFuzListener.java
@@ -1,6 +1,6 @@
 /*
- * This file is part of ProDisFuzz, modified on 21.09.18 21:33.
- * Copyright (c) 2013-2018 Volker Nebelung <vnebelung@prodisfuzz.net>
+ * This file is part of ProDisFuzz, modified on 4/8/19 7:41 PM.
+ * Copyright (c) 2013-2019 Volker Nebelung <vnebelung@prodisfuzz.net>
  * This work is free. You can redistribute it and/or modify it under the
  * terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See the COPYING file for more details.
@@ -20,7 +20,7 @@ public interface IFuzListener {
      * process the command for whatever reason it must throw an ProtocolExecutionException.
      *
      * @param data the data that is used as input for the fuzzing target
-     * @return the data collected by the server during the execution
+     * @return the data that was returned by the fuzzing target
      * @throws ProtocolExecutionException if the listener could not process the command
      */
     Map<String, String> receive(byte... data) throws ProtocolExecutionException;
